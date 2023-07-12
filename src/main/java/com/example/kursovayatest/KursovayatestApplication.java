ff387903ab93fc5d1f56644a73d889f073126eeb
larchick <liliyagreis@gmail.com>
2023-07-12 19:47:53 +0300

    1

---
+++ src/main/java/com/example/kursovayatest/KursovayatestApplication.java
@@ -0,0 +1,13 @@
+package com.example.kursovayatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KursovayatestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KursovayatestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kursovayatest/KursovayatestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kursovayatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KursovayatestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
